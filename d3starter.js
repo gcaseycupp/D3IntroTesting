9c6a82708e153c42085a059c498920ecde7a3ba7
casey <thecupps@gmail.com>
2016-07-01 14:20:32 -0600

    pluralsight basics d3

--- d3starter.js
+++ d3starter.js
@@ -1,11 +1,37 @@
 
+var w=500
+var h=100;
+var padding=2;
+var dataset = [5,10,15,20,25];
+var svg = d3.select("body")
+	.append("svg")
+		.attr("width",w)
+		.attr("height",h);
+
+svg.selectAll("rect")
+	.data(dataset)
+	.enter()
+	.append("rect")
+		.attr("x",function(d,i){ 
+				return (i* (w/dataset.length));
+		})
+		.attr("y",function(d){
+			return (h-(d*4));
+		})
+		.attr("width",function(d,i){
+				return (w/dataset.length - padding)
+		})
+		.attr("height",function(d){
+			return d*4;
+		});
+
 d3.select("body")
 .append("svg")
 .append("rect")
 	.attr("width",50)
 	.attr("height",200)
 	.style("fill","blue");
-	
+
 d3.select("body")
 	.append("svg")
 		.attr("width",50)
@@ -25,5 +51,5 @@ d3.select("body")
 		.attr("y",25)		
 		.text("easy");
 
-alert("b");
+//alert("b");
 		
